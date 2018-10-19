a2b37eb1eaeae677c5d51299e3a77290a28c68d6
DanE <edwardsdan001@gmail.com>
2018-10-19 08:52:12 -0400

    Added first condition

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -15,6 +15,13 @@ namespace TestGit
                 Console.WriteLine("Hi there! Enter a number between 1 and 100!");
                 string input = Console.ReadLine();
                 int number = int.Parse(input);
+                if (number % 2 == 0)
+                {
+                    if (number >= 2 && number < 25)
+                    {
+                        Console.WriteLine("Even and less than 25");
+                    }
+                }
             }
 
         }
